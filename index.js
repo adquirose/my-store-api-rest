796d06228dfad1705b5e10bcfeb8fc5e145821be
Arturo Quiros <adquirose@gmail.com>
2023-03-11 21:35:59 -0300

    demo api rest

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler')
 const app = express();
 const port = 3005;
+const whitelist = [
+  'localhost:8080',
+  'https://myapp.com'
+]
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null, true)
+    }else{
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors(options));
 app.use(express.json()) // para recibir informacion de tipo JSON por post
 app.get('/', (req, res) => {
   res.send("Hola mi server en express")
