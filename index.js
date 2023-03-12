7efc438034d9686b43373ec696dad8438853a60d
Arturo Quiros <adquirose@gmail.com>
2023-03-11 23:22:26 -0300

    cambio puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const routerApi = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler')
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3000;
 const whitelist = [
   'http://localhost:8080/',
   'https://206.62.252.185'
