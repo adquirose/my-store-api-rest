794083ae3416901fea5ca365af6ef0dfff5f39fe
Arturo Quiros <adquirose@gmail.com>
2023-04-05 21:04:50 -0400

    migrations 1

--- db/migrations/20230331131931-categories.js
+++ db/migrations/20230331131931-categories.js
@@ -1,9 +1,33 @@
 'use strict';
-const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model')
+const { CATEGORY_TABLE } = require('../models/category.model');
+const { DataTypes, Sequelize } = require('sequelize');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema)
+    await queryInterface.createTable(CATEGORY_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name:{
+        allowNull:false,
+        unique:true,
+        type:DataTypes.STRING,
+      },
+      image:{
+        allowNull:false,
+        type:DataTypes.STRING,
+      },
+      createdAt:{
+        allowNull:false,
+        type:DataTypes.DATE,
+        field:'create_at',
+        defaultValue: Sequelize.NOW
+      },
+    })
   },
 
   async down (queryInterface) {
