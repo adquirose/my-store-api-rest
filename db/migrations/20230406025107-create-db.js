7384ab9c1886d2b260ef4f22788b73c73a3dd82e
Arturo Quiros <adquirose@gmail.com>
2023-04-05 23:03:58 -0400

    migrations create db

---
+++ db/migrations/20230406025107-create-db.js
@@ -0,0 +1,53 @@
+'use strict';
+const { DataTypes, Sequelize } = require('sequelize');
+const { CategorySchema, CATEGORY_TABLE } = require('./../models/category.model');
+const { ProductSchema, PRODUCT_TABLE } = require('./../models/product.model');
+const { UserSchema, USER_TABLE } = require('./../models/user.model');
+const { CustomerSchema, CUSTOMER_TABLE } = require('./../models/customer.model');
+const { ORDER_TABLE } = require('./../models/order.model');
+const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('./../models/order-product.model');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId:{
+        field:'customer_id',
+        allowNull:true,
+        type:DataTypes.INTEGER,
+        references:{
+          model :CUSTOMER_TABLE,
+          key:'id'
+        },
+        onUpdate:'CASCADE',
+        onDelete:'SET NULL'
+      },
+
+      createdAt:{
+        allowNull:false,
+        type:DataTypes.DATE,
+        field:'create_at',
+        defaultValue: Sequelize.NOW
+      },
+    });
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+  }
+};
